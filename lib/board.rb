989558cd984bcecda4782d63716c78a8a378578f
murgraydj <murgraydj@gmail.com>
2023-10-10 19:37:24 -0400

    Adds board_setup and print_board method
    Both methods are incomplete, but working as-is.

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,26 @@
 
 class Board
   attr_accessor :board
-  ROWS = 8
-  COLUMNS = 8
+  ROWS = 10
+  COLUMNS = 10
 
   def initialize
-    @board = Array.new(ROWS){Array.new(COLUMNS){"\u2237"}}
+    @board = board_setup
+  end
+
+  def board_setup 
+    new_board = Array.new(ROWS){Array.new(COLUMNS){"\u2237"}}
+    files = [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
+    ranks = [" ", 8, 7, 6, 5, 4, 3, 2, 1, " "]
+    new_board[0] = files.map(&:clone)
+    new_board[9] = files.map(&:clone)
+    new_board
+  end
+
+  def print_board
+    @board.each do |row| 
+      p row
+    end
   end
 end
 
