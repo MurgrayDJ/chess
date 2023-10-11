b098efff00cc08b008aaefb39bed0e75136ac0a0
murgraydj <murgraydj@gmail.com>
2023-10-11 19:50:36 -0400

    Adds king's eligible moveset as a constant

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -6,6 +6,9 @@ class King
   attr_accessor :name
   attr_accessor :start_pos
   attr_accessor :current_pos
+  MOVES = [[1,-1], [1,0], [1,1],
+           [0,-1], [0,1], 
+           [-1,-1], [-1,0], [-1,1]].freeze
   
   def initialize(type, start_pos)
     @type = type
@@ -14,7 +17,7 @@ class King
   end
 
   def set_symbol
-    if type == :black
+    if @type == :black
       @symbol = "\u265a"
     else
       @symbol = "\u2654"
