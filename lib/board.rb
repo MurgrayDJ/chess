bfc3e5e8106344c179044a4cefcf16437a473355
murgraydj <murgraydj@gmail.com>
2023-10-11 18:28:30 -0400

    Hardcodes board creation.
    It was done before as a series of methods, but hardcoding it just makes
    more sense in this case. The methods have much greater time complexity,
    including iterating over the whole board several times.

--- lib/board.rb
+++ lib/board.rb
@@ -3,43 +3,22 @@ class Board
   attr_accessor :board
   ROWS = 10
   COLUMNS = 10
+  DOTS = "\u2237"
 
   def initialize
-    @board = board_setup
-  end
-
-  def board_setup 
-    new_board = Array.new(ROWS){Array.new(COLUMNS){"\u2237"}}
-    add_files(new_board)
-    add_ranks(new_board)
-    add_spaces(new_board)
-    new_board
-  end
-
-  def add_files(new_board)
-    files = [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
-    new_board[0] = files.map(&:clone)
-    new_board[9] = files.map(&:clone)
-  end
-
-  def add_ranks(new_board)
-    ranks = [" ", 8, 7, 6, 5, 4, 3, 2, 1, " "]
-    ranks.each_with_index do |rank, idx|
-      new_board[idx][0] = rank
-      new_board[idx][9] = rank
-    end
-  end
-
-  def add_spaces(new_board)
-    (8).downto(1) do |row|
-      (8).downto(1) do |col|
-        if col.odd? && row.even?
-          new_board[row][col] = " "
-        elsif col.even? && row.odd?
-          new_board[row][col] = " "
-        end
-      end
-    end
+    # @board = board_setup
+    @board = [
+      [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "],
+      [8, DOTS, " ", DOTS, " ", DOTS, " ", DOTS, " ", 8],
+      [7, " ", DOTS, " ", DOTS, " ", DOTS, " ", DOTS, 7],
+      [6, DOTS, " ", DOTS, " ", DOTS, " ", DOTS, " ", 6],
+      [5, " ", DOTS, " ", DOTS, " ", DOTS, " ", DOTS, 5],
+      [4, DOTS, " ", DOTS, " ", DOTS, " ", DOTS, " ", 4],
+      [3, " ", DOTS, " ", DOTS, " ", DOTS, " ", DOTS, 3],
+      [2, DOTS, " ", DOTS, " ", DOTS, " ", DOTS, " ", 2],
+      [1, " ", DOTS, " ", DOTS, " ", DOTS, " ", DOTS, 1],
+      [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
+    ]
   end
 
   def print_board
@@ -70,6 +49,40 @@ class Board
       if row_num == 8 then print bottom_line end
     end
   end
+
+    # def board_setup 
+  #   new_board = Array.new(ROWS){Array.new(COLUMNS){"\u2237"}}
+  #   add_files(new_board)
+  #   add_ranks(new_board)
+  #   add_spaces(new_board)
+  #   new_board
+  # end
+
+  # def add_files(new_board)
+  #   files = [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
+  #   new_board[0] = files.map(&:clone)
+  #   new_board[9] = files.map(&:clone)
+  # end
+
+  # def add_ranks(new_board)
+  #   ranks = [" ", 8, 7, 6, 5, 4, 3, 2, 1, " "]
+  #   ranks.each_with_index do |rank, idx|
+  #     new_board[idx][0] = rank
+  #     new_board[idx][9] = rank
+  #   end
+  # end
+
+  # def add_spaces(new_board)
+  #   (8).downto(1) do |row|
+  #     (8).downto(1) do |col|
+  #       if col.odd? && row.even?
+  #         new_board[row][col] = " "
+  #       elsif col.even? && row.odd?
+  #         new_board[row][col] = " "
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 # board_class = Board.new()
