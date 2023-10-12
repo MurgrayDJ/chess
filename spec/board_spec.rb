5b6b1566cc2b9be7605c5d350bb9d8e4a52c625a
murgraydj <murgraydj@gmail.com>
2023-10-12 19:07:36 -0400

    Tests '#update_old_spot' for odd sum spot
    This test checks for when the sum of the coordinates is odd, meaning
    there should be a space where the spot is.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/board.rb'
 
 RSpec.describe Board do
   before { @board_class = described_class.new }
+  DOTS = "\u2237"
 
   describe "#print_board" do
     context "board is empty" do 
@@ -12,15 +13,28 @@ RSpec.describe Board do
   end
 
   describe "#move_piece" do
-    context "king is in starting position" do
+    context "king is moving from e8 [1,5] to f7 [2,6]" do
       let(:king) {double(symbol: "\u265a", current_pos: [1,5])}
-      it "should have the king in a different position" do
+      it "should have the king in f7 [2,6]" do
         @board_class.print_board
         @board_class.board[1][5] = king
         @board_class.print_board
         @board_class.move_piece([1,5], [2,6])
         @board_class.print_board
         expect(@board_class.board[2][6]).to eq(king)
+      end
+    end
+  end
+
+  describe "#update_old_spot" do
+    context "there's a king symbol on g3 [6,7]" do
+      it "should remove the symbol and put a space there" do
+        sym = "\u265a"
+        @board_class.board[6][7] = sym
+        @board_class.print_board
+        @board_class.update_old_spot(6,7)
+        @board_class.print_board
+        expect(@board_class.board[6][7]).to eq(" ")
       end
     end
   end
