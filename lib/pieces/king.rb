8a53ed5237b0ad61a491d2801bc2f4983c004eb1
murgraydj <murgraydj@gmail.com>
2023-10-14 02:01:10 -0400

    Creates Pieces.rb and makes King it's sublcass
    The idea is that all the future pieces will also inherit from the Pieces
    class.

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,32 +1,18 @@
+require_relative "pieces.rb"
 
-
-class King
-  attr_accessor :symbol
-  attr_accessor :type
-  attr_accessor :name
-  attr_accessor :start_pos
-  attr_accessor :current_pos
-  MOVES = [[1,-1], [1,0], [1,1],
-           [0,-1], [0,1], 
-           [-1,-1], [-1,0], [-1,1]].freeze
+class King < Pieces
+  MOVES = [[1,-1], [1,0], [1,1],[0,-1], 
+          [0,1], [-1,-1], [-1,0], [-1,1]].freeze
   
   def initialize(type, start_pos)
-    @type = type
-    @start_pos = start_pos
-    @current_pos = start_pos
-    set_symbol
+    super
   end
 
   def set_symbol
-    if @type == :black
-      @symbol = "\u265a"
-    else
-      @symbol = "\u2654"
-    end
+    super
   end
 
   def get_moves
-    possible_moves = MOVES.map {|move| [current_pos[0] + move[0], current_pos[1] + move[1]]}
-                      .select {|move| move[0].between?(1,8) && move[1].between?(1,8)}
+    super(MOVES)
   end
 end
---
+++ lib/pieces/pieces.rb
@@ -0,0 +1,29 @@
+
+
+class Pieces
+  attr_accessor :symbol
+  attr_accessor :type
+  attr_accessor :name
+  attr_accessor :start_pos
+  attr_accessor :current_pos
+
+  def initialize(type, start_pos)
+    @type = type
+    @start_pos = start_pos
+    @current_pos = start_pos
+    set_symbol
+  end
+
+  def set_symbol
+    if @type == :black
+      @symbol = "\u265a"
+    else
+      @symbol = "\u2654"
+    end
+  end
+
+  def get_moves(move_list)
+    possible_moves = move_list.map {|move| [current_pos[0] + move[0], current_pos[1] + move[1]]}
+                      .select {|move| move[0].between?(1,8) && move[1].between?(1,8)}
+  end
+end
