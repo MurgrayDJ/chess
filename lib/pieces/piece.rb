8ea030fcacf2f74be6a8bf7ac7e9378b9cad54b6
murgraydj <murgraydj@gmail.com>
2023-10-16 16:00:29 -0400

    Creates hash for all pieces' moves in piece
    This means there won't need to be individual classes dedicated to the
    different types of pieces, since the moves list is the only thing
    differentiating them.
    
    I noticed after creating the original Pieces class and changing King.rb
    to inherit from it, that there wasn't much point in having the King
    class, since Pieces (now Piece) could do everything for it and the other
    pieces.

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -7,6 +7,10 @@ class Piece
   attr_accessor :name
   attr_accessor :start_pos
   attr_accessor :current_pos
+  MOVES = {
+    king: [[1,-1], [1,0], [1,1],[0,-1], 
+    [0,1], [-1,-1], [-1,0], [-1,1]]
+  }
 
   def initialize(color, type, start_pos)
     @color = color
@@ -24,7 +28,8 @@ class Piece
     end
   end
 
-  def get_moves(move_list)
+  def get_moves
+    move_list = MOVES[@type]
     possible_moves = move_list.map {|move| [current_pos[0] + move[0], current_pos[1] + move[1]]}
                       .select {|move| move[0].between?(1,8) && move[1].between?(1,8)}
   end
