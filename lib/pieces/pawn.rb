dccd79a2e56237d91fa774659262ebe2ed795f56
murgraydj <murgraydj@gmail.com>
2023-10-24 16:39:57 -0400

    Writes Pawn to inherit from Pieces, adds Pawn test

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,4 +1,17 @@
+require_relative '../piece.rb'
 
-class Pawn
+class Pawn < Piece
+  MOVES = [[1,0]].freeze
 
+  def initialize(color, start_pos)
+    super(color, start_pos, :pawn)
+  end
+
+  def set_symbol
+    super
+  end
+
+  def get_moves
+    super(MOVES)
+  end
 end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/pieces/pawn.rb'
+
+RSpec.describe Pawn do
+
+  describe "#get_moves" do
+    before {@w_pawn = described_class.new(:white, [7,6])}
+
+    #Pawn
+    context "f2 [7,6] pawn is in starting position with no piece in front" do
+      it "should return a list with 1 move" do
+        expect(@w_pawn.get_moves).to match_array([[8,6]])
+      end
+    end
+  end
+end
