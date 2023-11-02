c63ece270b69336884be1637dd894aa0e901ecf1
murgraydj <murgraydj@gmail.com>
2023-11-01 20:14:33 -0400

    Tests if #get_moves works with hashes
    The previous tests for arrays are all still passing.

--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -3,21 +3,18 @@ require_relative '../../lib/pieces/queen.rb'
 RSpec.describe Queen do
   before {@b_queen = described_class.new(:black, [1,4])}
 
-  # describe "#get_moves" do
-  #   before {@w_pawn = described_class.new(:white, [7,6])}
-
-  #   #Pawn
-  #   context "f2 [7,6] pawn is in starting position with no piece in front" do
-  #     it "should return a list with 1 move" do
-  #       expect(@w_pawn.get_moves).to match_array([[8,6]])
+  # describe "#generate_moves" do
+  #   context "New queen is created" do
+  #     it "should generate her whole move list" do
+  #       expect(@b_queen.moves.length).to eq(56)
   #     end
   #   end
   # end
 
-  describe "#generate_moves" do
-    context "New queen is created" do
-      it "should generate her whole move list" do
-        expect(@b_queen.moves.length).to eq(56)
+  describe "#get_moves" do
+    context "b_queen is in starting position" do
+      it "should generate a hash list with 21 moves" do
+        expect(@b_queen.get_moves.values.reduce([], :concat).length).to eq(21)
       end
     end
   end
