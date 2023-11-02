e8f22e7bb9c6be9649d459540137d24fcf3d799b
murgraydj <murgraydj@gmail.com>
2023-11-02 12:41:26 -0400

    Creates rook_spec with starting move test
    The tests checks that the moveset for a white rook on it's starting
    space is properly generated

---
+++ spec/pieces/rook_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../lib/pieces/rook.rb'
+
+RSpec.describe Rook do
+  before {@w_rook = described_class.new(:white, [8,1])}
+
+  describe "#get_moves" do
+    context "w_rook is in starting position" do
+      it "should generate a hash list with 14 moves" do
+        expect(@w_rook.get_moves.values.reduce([], :concat).length).to eq(14)
+      end
+    end
+  end
+end
