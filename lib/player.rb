a4a9a635bddbef68c19d2ebe84c2494fa4d48017
murgraydj <murgraydj@gmail.com>
2023-11-04 11:32:24 -0400

    Defines initialize and adds attributes

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,14 @@
 
 class Player
+  attr_accessor :name
+  attr_accessor :pieces
+  attr_accessor :pieces_taken
+  attr_reader :color
 
+  def initialize(name, color)
+    @name = name
+    @color = color
+    @pieces = []
+    @pieces_taken = []
+  end
 end
