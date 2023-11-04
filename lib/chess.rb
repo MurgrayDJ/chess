0478ef5f3cb8e7e5a47a8230a82a3b87426fe64f
murgraydj <murgraydj@gmail.com>
2023-11-04 11:46:27 -0400

    Adds functionality to create players
    This includes data validation. This content is the same as from the
    Connect Four project.

--- lib/chess.rb
+++ lib/chess.rb
@@ -11,6 +11,7 @@ class Chess
   def run_game
     print_title
     print_rules
+    create_players
   end
 
   def print_title
@@ -33,6 +34,9 @@ class Chess
     puts "\nHow to play: "
     puts " #{DOTS} If you don't already know how to play chess, please reference other "
     puts "   material for overall instructions"
+    puts " #{DOTS} The White side is whichever one that starts on ranks 1 and 2 "
+    puts "   (towards the bottom of the screen.)"
+    puts " #{DOTS} Player 1 is White and goes first. Then players alternate turns."
     puts " #{DOTS} When it's your turn to play, please enter the file then rank of the "
     puts "   piece you would like to move. (e.g., e2, f7)"
     puts " #{DOTS} Then enter the file then rank of the space you'd like to move your "
@@ -40,5 +44,50 @@ class Chess
     puts " #{DOTS} Type 'exit' to leave at any time."
     puts " #{DOTS} Type 'help' at any time to repeat this message.\n\n"
     puts "#{DOTS} " * 21 
+  end
+
+  def create_players
+    puts "Welcome Players! Please enter your names: "
+    @player1 = Player.new(get_names(1), :white)
+    @player2 = Player.new(get_names(2), :black)
+  end
+
+  def get_names(player_num)
+    name = nil
+    while name.nil?
+      name = confirm_name(player_num)
+    end
+    name
+  end
+
+  def confirm_name(player_num)
+    print "Player #{player_num} name: "
+    name = gets.chomp
+    confirmation = "Confirm name #{name}? (Y/N): "
+    response = get_valid_data(confirmation, nil, ["Y", "N"])
+    if response == "N" then name = nil end
+    name
+  end
+
+  def get_valid_data(prompt, response, valid_responses) 
+    if response.nil?
+      print prompt
+      response = gets.chomp
+    else
+      response = response.upcase
+      valid_responses.each do |valid_response|
+        if response == valid_response
+          return response
+        elsif response == "EXIT"
+          puts "Thank you for playing!"
+          exit!
+        elsif response == "HELP"
+          print_rules
+          break
+        end
+      end
+      response = nil
+    end
+    response = get_valid_data(prompt, response, valid_responses)  
   end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -20,4 +20,28 @@ RSpec.describe Chess do
       end
     end
   end
+
+  describe '#get_names' do
+  context 'player1 enters name then confirms it' do
+    it 'should return the player 1 name' do
+      allow(@game).to receive(:gets).and_return("Bob\n", "Y\n")
+      expect(@game.get_names(1)).to eq("Bob")
+    end
+  end
+
+  context 'player2 enters name then changes it' do
+    it 'should return the second player 2 name' do
+      allow(@game).to receive(:gets).and_return("Mumu\n", "n\n", "Linda\n", "Y\n")
+      expect(@game.get_names(2)).to eq("Linda")
+    end
+  end
+
+  context 'player1 enters invalid text for name confirmation' do
+    it 'should still return the first name after eventual yes' do
+      allow(@game).to receive(:gets).and_return(
+        "Bob\n", "kdyfj\n", "2836%(^*\n", "banana34\n", "Y\n")
+      expect(@game.get_names(1)).to eq("Bob")
+    end
+  end
+end
 end
