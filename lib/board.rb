299f4353ececfcee91e88c812adb600d43996184
murgraydj <murgraydj@gmail.com>
2023-11-10 21:52:12 -0500

    Removes extra space being printed in col 1

--- lib/board.rb
+++ lib/board.rb
@@ -24,10 +24,8 @@ class Board
   end
 
   def print_board
-    # top_line = " " + ("_" * 39) + "\n"
-    # bottom_line = (" \u0305" * 39) + "\n"
-    top_line = " ___    ________________________________    ___ \n"
-    bottom_line = " ̅ ̅ ̅     ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅     ̅ ̅ ̅  \n"
+    top_line = " ___    ________________________________   ___ \n"
+    bottom_line = " ̅ ̅ ̅     ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅    ̅ ̅ ̅  \n"
     print top_line 
     print_rows(top_line, bottom_line)
     print bottom_line
@@ -42,7 +40,7 @@ class Board
         if col_num != 1 && col_num != 9
           print "| #{val} "
         elsif col_num == 1
-          print "|  | #{val}  "
+          print "|  | #{val} "
         elsif col_num == 9
           print "|  | #{val} "
         end
@@ -119,40 +117,6 @@ class Board
       delete_moves(piece, moves)
     end
   end
-  ### OLD BOARD SETUP METHODS
-  # def board_setup 
-  #   new_board = Array.new(ROWS){Array.new(COLUMNS){"\u2237"}}
-  #   add_files(new_board)
-  #   add_ranks(new_board)
-  #   add_spaces(new_board)
-  #   new_board
-  # end
-
-  # def add_files(new_board)
-  #   files = [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
-  #   new_board[0] = files.map(&:clone)
-  #   new_board[9] = files.map(&:clone)
-  # end
-
-  # def add_ranks(new_board)
-  #   ranks = [" ", 8, 7, 6, 5, 4, 3, 2, 1, " "]
-  #   ranks.each_with_index do |rank, idx|
-  #     new_board[idx][0] = rank
-  #     new_board[idx][9] = rank
-  #   end
-  # end
-
-  # def add_spaces(new_board)
-  #   (8).downto(1) do |row|
-  #     (8).downto(1) do |col|
-  #       if col.odd? && row.even?
-  #         new_board[row][col] = " "
-  #       elsif col.even? && row.odd?
-  #         new_board[row][col] = " "
-  #       end
-  #     end
-  #   end
-  # end
 end
 
 # board_class = Board.new()
